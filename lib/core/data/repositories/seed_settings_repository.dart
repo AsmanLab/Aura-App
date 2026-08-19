4ac5fb863f7adea1c1e0f8c9e8b22e440825e38c
Tantumgs <joingogames2@gmail.com>
2026-08-19 15:29:47 +0600

    feat: leaderboard self highlight as background only

--- lib/core/data/repositories/seed_settings_repository.dart
+++ lib/core/data/repositories/seed_settings_repository.dart
@@ -1,24 +1,8 @@
-import 'dart:async';
-
 import 'package:aura_app/core/domain/entities/notif_pref.dart';
 import 'package:aura_app/core/domain/repositories/settings_repository.dart';
 import '../seed/notif_prefs_seed.dart';
 
 class SeedSettingsRepository implements SettingsRepository {
-  static final StreamController<int?> _controller =
-      StreamController<int?>.broadcast()..add(0xFF8B5CF6);
-
-  @override
-  Stream<int?> watchLeaderboardHighlightColor() => _controller.stream;
-
-  @override
-  Future<int?> getLeaderboardHighlightColor() async => 0xFF8B5CF6;
-
-  @override
-  Future<void> setLeaderboardHighlightColor(int colorValue) async {
-    _controller.add(colorValue);
-  }
-
   @override
   Future<List<NotifPref>> getNotifPrefs() async => NotifPrefsSeed.prefs;
 
